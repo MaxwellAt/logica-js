9c61a085d2a560de1493c091b4fba78a43ee6214
Jêfferson Maxwell <96148846+MaxwellAt@users.noreply.github.com>
2024-03-21 11:31:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 5000;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
-
-// enquanto chute não for igual ao n.s.
-while (chute != numeroSecreto) {
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-    // se chute for igual ao número secreto
-    if (chute == numeroSecreto) {
-        break;
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else {
-            alert(`O número secreto é maior que ${chute}`);
-        }
-        // tentativas = tentativas + 1;
-        tentativas++;
-    }
+alert("Boas vindas ao jogo de adivinhação!\n Esse jogo consiste em voce acertar um numero secreto.\n Boa sorte!");
+let numeroSecreto = Math.floor(Math.random() * 11);
+let tentativas = 3;
+while (tentativas > 0) {
+  let chute = parseInt(prompt("Digite um número entre 0 e 10"));
+  if (numeroSecreto == chute) {
+    alert("Parabéns! Você acertou!");
+    break;
+  } else if (chute > numeroSecreto) {
+    alert("O número secreto é menor que o chute");
+    tentativas--;
+  } else if (chute < numeroSecreto) {
+    alert("O número secreto é maior que o chute");
+    tentativas--;
+  }
 }
-
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
-
-// if (tentativas > 1) {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-// } else {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-// }
+if (tentativas == 0) {
+  alert("Suas tentativas acabaram! O número secreto era " + numeroSecreto);
+  window.location.href = "https://www.google.com.br/";
+}
